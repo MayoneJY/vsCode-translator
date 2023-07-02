b594dcf36b458950ac8a8089a5848a721eec7785
MayoneJY <mayone6063@kakao.com>
2023-07-02 21:25:14 +0900

    feat: 번역기능 생성
    temp == 1 -> 카멜 기법
    temp == 2 -> 스네이크 기법
    
    현재는 하드코딩 상황
    이후 교체 예정

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,72 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
 import * as vscode from 'vscode';
+import express from 'express';
+var app = express();
+var temp = 2;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "translator" is now active!');
+	console.log('축하합니다. "translator" 확장이 이제 활성화되었습니다!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('translator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from translator!');
+	let disposable = vscode.commands.registerCommand('translator.translate', () => {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const word = document.getText(selection);
+
+			var query = word;
+			var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+			var request = require('request');
+			var options = {
+				url: api_url,
+				form: {'source':'ko', 'target':'en', 'text':query},
+				headers: {'X-Naver-Client-Id':clientId, 'X-Naver-Client-Secret': clientSecret}
+				};
+			request.post(options, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					// 번역된 결과를 불러오기
+					let result = JSON.parse(body).message.result.translatedText;
+
+					result = result.toLowerCase();
+					// 설정된 기법에 따라 변환
+					if (temp == 1) {
+						// 카멜 기법
+						result = result.replace(" ", "_");
+					}
+					else if (temp == 2) {
+						// 스네이크 기법
+						var resultWords = result.split(" ");
+						for (var i = 1; i < resultWords.length; i++) {
+							resultWords[i] = resultWords[i][0].toUpperCase() + resultWords[i].substr(1);
+						}
+						result = resultWords.join("");
+					}
+
+					// 변환된 결과를 에디터에 적용
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, result);
+					});
+				} else {
+					console.log('error = ' + response.statusCode);	
+				}
+			});
+			app.listen(3000, function () {
+			console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
+			});
+
+
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
